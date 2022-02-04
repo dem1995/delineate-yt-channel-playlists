d59b5384957ddc90f91dbd1419982a911d98b545
DEMcKnight <belmont95@live.com>
2022-02-03 20:30:39 -0700

    adjusting the remove_misc_files script in case people decide to download vids/music with it

--- remove_misc_files.py
+++ remove_misc_files.py
@@ -6,7 +6,7 @@ parser.add_argument("directory_name")
 args = parser.parse_args()
 for root, dirs, files in os.walk("."):
     for file in files:
-        if not file.startswith("0 ") and not file.endswith(".description") and not file.endswith(".txt"):
+        if not file.startswith("0 ") and not file.endswith(".description") and file.endswith(".info.json"):
             # Remove the file
             filepath = os.path.join(root, file)
             os.remove(filepath)
